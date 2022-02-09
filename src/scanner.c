4c7e45922a3d4f1ad7c0d2351f9ba073a98b8a0f
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2022-02-09 23:42:34 +0100

    fix parser no cmd_newline on ) or \}

--- src/scanner.c
+++ src/scanner.c
@@ -14,17 +14,17 @@ void tree_sitter_nu_external_scanner_deserialize(void *p, const char *b, unsigne
 bool tree_sitter_nu_external_scanner_scan(
     void *payload, TSLexer *lexer, const bool *valid_symbols
 ) {
+    while (lexer->lookahead == ' ') {lexer->advance(lexer, true);}
     if (valid_symbols[_CMD_NEWLINE]) {
-        if (!(
-            lexer->lookahead == 0 ||
-            lexer->lookahead != '\n' // || 
-            /* lexer->lookahead != ')' || */ 
-            /* lexer->lookahead != '}' */
-        ) || lexer->lookahead == 0) {
+        if (lexer->lookahead == 0 
+            || lexer->lookahead == '}'  
+            || lexer->lookahead == ')' 
+            || lexer->lookahead == '\n'
+            || lexer->lookahead == '\r' // \r\n for windows newlines
+            ){
             lexer->result_symbol = _CMD_NEWLINE;
             return true;
         }
     }
-
     return false;
 }
